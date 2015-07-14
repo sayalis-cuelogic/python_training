dcebfa94d7e67bafc9c3587956b6aa352d4d2f8f
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-07-14 17:07:16 +0530

    Examples of Exercise 9(formatting of a string(\n))

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here is the days " , days
+print "Here is the months " , months
+
+print """
+	Here we can use 3 double quotes.
+	For writing multiple lines.
+	Above list gives the names of days 
+	and names of months....
+""" 
