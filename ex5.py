b96091cf07b9c0ef56e81ed7ec35966014337ec1
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-07-14 14:50:02 +0530

    Examples of Exercise 5(Variables & strings)

---
+++ ex5.py
@@ -0,0 +1,12 @@
+name = 'Sayali'
+age = 21
+weight = 57
+height = 5.6
+eyes = 'blue'
+hair = 'brown'
+
+print "Hiii I am %s." % name
+print "My age is %d." % age
+print "My height is %.1f and weight is %d ." % (height,weight)
+
+print "If I add my age %d , height %d and weight %d I get %d." % (age,height,weight,age + height + weight)
