a916e18e7caad0f9a5a26ae228f5b7a4ccc543e5
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-07-14 14:07:07 +0530

    Examples of Exercise 3

---
+++ ex3.py
@@ -0,0 +1,14 @@
+print "Numbers and Math"
+print "I will count my fruits"
+print "Apple ", 35 + 20 / 5
+print "Mango ", 20 + 30 / 3
+
+print "Is it true that 5 + 7 > 5 - 3 "
+print 5 + 7 > 5 - 3
+
+print "What is 55 + 15 ", 55 + 15 
+print "What is 25 - 35 ", 25 - 35
+
+print "10 > -7 Is it greater?", 10 > -7
+print "20 >= -2 Is it greater or equal?", 20 >= -2
+print "7 <= -1 Is it less or equal?", 7 <= -1
