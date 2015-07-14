fa139bd7f0d59044aa3f1960b6c66aff7b457fbe
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-07-14 18:04:25 +0530

    Examples of Exercise 13(taking input from command line)

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script,first,second,third = argv
+
+print "Script name is %s" % script
+print "First variable is %s" % first
+print "Second variable is %s" % second
+print "Third variable is %s" % third
+ 
