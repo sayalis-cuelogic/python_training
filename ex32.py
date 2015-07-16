699c73bb7f2e0c271d7ebcd22c36a37d664ff6ad
sayalis-cuelogic <sayali.shete@cuelogic.co.in>
2015-07-16 18:07:53 +0530

    Exercise 32 use of 'for loop'

---
+++ ex32.py
@@ -0,0 +1,24 @@
+count = [1, 2, 3, 4, 5]
+fruits = ['Apple', 'Mango', 'Banana', 'Orange', 'Pears']
+change = [1, 'Sayali', 2, 'Sonali', 3, 'Deepa', 4, 'Shruti']
+
+for number in count:
+	print "This is the count : %d " % number
+
+for fruit in fruits:
+	print "A fruit is %s" % fruit
+
+for i in change:
+	print "I got : %r" % i
+
+elements = []
+
+print "Adding elements..."
+
+for  e in range(1,6):
+	print "Enter %d element for the list" % e
+	data = raw_input()
+	elements.append(data)
+
+for e in elements:
+	print "element is %r" % e
